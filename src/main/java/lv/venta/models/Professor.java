f66d4d2bf61d51523d6be857a2da21d7f4503775
localuser <localuser@VEA.LV>
2023-05-18 13:48:38 +0300

    Created and added Proffesor class

---
+++ src/main/java/lv/venta/models/Professor.java
@@ -0,0 +1,46 @@
+package lv.venta.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table(name = "proffesor_table")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Professor {
+	
+	@Column(name = "id_p")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private long id_p;
+	
+	@Column(name = "Name")
+	@NotNull
+	@Size(min = 3, max = 150)
+	@Pattern(regexp = "[A-Z]{1}[a-z\\ ]+")
+	private String Name;
+	
+	@Column(name = "Surname")
+	@NotNull
+	@Size(min = 3, max = 150)
+	@Pattern(regexp = "[A-Z]{1}[a-z\\ ]+")
+	private String Surname;
+	
+	
+	private Degree Degree;
+}
